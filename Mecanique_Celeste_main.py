452b117946f0385e100e59a39e726bf7768504f7
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-03-13 16:03:07 -0400

    Create Mecanique_Celeste_main.py

---
+++ Mecanique_Celeste_main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+donnee_probleme: dict = {
+    "label_i": ["A", "B", "C"],
+    "m_i": np.array([3, 4, 5]),
+    "x_i(0)": np.array([1, -2, 1]),
+    "y_i(0)": np.array([3, -1, -1]),
+}
+
+
+if __name__ == '__main__':
+    pass
