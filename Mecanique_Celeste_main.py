01495ea7d3899b3575912538be0e33faab5b62ae
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-03-17 15:15:42 -0400

    simulations insane

--- Mecanique_Celeste_main.py
+++ Mecanique_Celeste_main.py
@@ -1,13 +1,43 @@
 import numpy as np
 
-donnee_probleme: dict = {
-    "labels": ["A", "B", "C"],
-    "m_i": np.array([3, 4, 5]),
-    "r_0": np.array([
-        [1, 3],
-        [-2, -1],
-        [1, -1]
-    ]),
+donnee_problemes: dict = {
+    "a": {
+        "labels": ["A", "B", "C"],
+        "t": [0, 1],
+        "m_i": np.array([3, 4, 5]),
+        "r_0": np.array([
+            [1, 3],
+            [-2, -1],
+            [1, -1]
+        ]),
+        "v_0": np.zeros((3, 2)),
+    },
+    "b": {
+        "labels": ["A", "B", "C"],
+        "t": [0, 10],
+        "m_i": np.array([3, 4, 5]),
+        "r_0": np.array([
+            [1, 3],
+            [-2, -1],
+            [1, -1]
+        ]),
+        "v_0": np.zeros((3, 2)),
+    },
+    "c": {
+        "labels": ["A", "B", "C"],
+        "t": [0, 15],
+        "m_i": np.array([1, 1, 1]),
+        "r_0": np.array([
+            [3.3030197, -0.82771837],
+            [-3.3030197, 0.82771837],
+            [0.0, 0.0]
+        ]),
+        "v_0": np.array([
+            [1.587433767, 1.47221479],
+            [1.587433767, 1.47221479],
+            [-3.174867535, -2.94442961]
+        ]),
+    }
 }
 
 
@@ -40,10 +70,10 @@ def Fg(positions: np.ndarray, masses: np.ndarray) -> np.ndarray:
         constante_de_poids = constantes_de_masse*(1/constante_de_normalisation)  # (m_i*m_j)/|r_i - r_j|^3
         constante_de_poids[constante_de_poids == np.inf] = 0   # on remplace les inf pour des 0
 
-        # on reshape la matrice avoir de pouvoir faire une multiplication element wise
+        # on reshape la matrice afin de pouvoir faire une multiplication element wise
         constante_de_poids = np.tile(constante_de_poids.reshape((1, 3)), (2, 1)).transpose()
 
-        matrice_a_summer = np.multiply(constante_de_poids, position_relative)
+        matrice_a_summer = np.multiply(constante_de_poids, position_relative)   # (m_i*m_j) * (r_i - r_j)/|r_i - r_j|^3
         f_i.append(-constantes["G"]*np.sum(matrice_a_summer, axis=0))
     return np.array(f_i)
 
@@ -51,7 +81,8 @@ def Fg(positions: np.ndarray, masses: np.ndarray) -> np.ndarray:
 def resolution_probleme_trois_corps(masses: np.ndarray, **kwargs):
     [a, b] = kwargs.get("bornes", [0, 1])
     N = kwargs.get("resolution", 100)
-    r_0 = kwargs.get("conditions_initiales", np.zeros((3, 2)))
+    r_0 = kwargs.get("positions_initiales", np.zeros((3, 2)))
+    v_0 = kwargs.get("vitesses_initiales", np.zeros((3, 2)))
 
     h = (b - a) / N
     t_points = np.arange(a, b, h)
@@ -65,7 +96,7 @@ def resolution_probleme_trois_corps(masses: np.ndarray, **kwargs):
     g = lambda idx: v_points[idx]
 
     # initialisation au premier point à t_0
-    v_0 = f(r_0)
+    # v_0 = f(r_0)
     v_points.append(v_0)
     r_points.append(r_0)
 
@@ -77,30 +108,89 @@ def resolution_probleme_trois_corps(masses: np.ndarray, **kwargs):
 
     # résolution de l'équation différentielle avec saute mouton
     for i, t in enumerate(t_points[2:]):  # ici, l'index i est deja en retard de 2 sur l'index de t dans t_points
-        v_i = v_points[i] + h*f(r_points[i+1])
-        r_i = r_points[i] + h*g(i+1)
+        v_i = v_points[i] + h*f(r_points[i+1])  # v_i = v_{i-2} + h*f(v_{i-1}, t_{i-1})
+        r_i = r_points[i] + h*g(i+1)  # r_i = r_{i-2} + h*g(r_{i-1}, t_{i-1})
         v_points.append(v_i)
         r_points.append(r_i)
 
     return np.array(r_points), t_points
 
 
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
-
+def simulation_affichage_3D(matrice_de_position: np.ndarray, vecteur_temps: np.ndarray,
+                            titre: str = "Simulation_affichage_3D", labels=None):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
 
-    R, T = resolution_probleme_trois_corps(donnee_probleme["m_i"], conditions_initiales=donnee_probleme["r_0"],
-                                           bornes=[0, 1], resolution=100)
-    print(R.shape)
+    R, T = matrice_de_position, vecteur_temps
+
     for i in range(3):
-        ax.plot(xs=R[:, i, 0], ys=R[:, i, 1], zs=T, label=donnee_probleme["labels"][i])
+        ax.plot(xs=R[:, i, 0], ys=R[:, i, 1], zs=T, label=labels[i])
 
     ax.set_xlabel('x [unité de distance]')
     ax.set_ylabel('y [unité de distance]')
     ax.set_zlabel('t [unité de temps]')
     plt.legend()
     plt.grid()
+    plt.savefig(f"{titre}.png", dpi=300)
     plt.show()
+
+
+def simulaiton_animation_2D(matrice_de_position: np.ndarray, vecteur_temps: np.ndarray,
+                            titre: str = "animation_resolution_3_corps", labels=None):
+    plt.style.use('seaborn-pastel')
+
+    fig = plt.figure()
+    R, T = matrice_de_position, vecteur_temps
+
+    xlim = (min([np.min(R[:, i, 0]) for i in range(3)]), max([np.max(R[:, i, 0]) for i in range(3)]))
+    ylim = (min([np.min(R[:, i, 1]) for i in range(3)]), max([np.max(R[:, i, 1]) for i in range(3)]))
+
+    ax = plt.axes(xlim=xlim, ylim=ylim)
+
+    text_t = ax.text(xlim[0], ylim[1], f"t: {0}", horizontalalignment='right', verticalalignment='bottom')
+
+    line_0, = ax.plot([], [], 'o', lw=3, label=labels[0])
+    line_1, = ax.plot([], [], 'o', lw=3, label=labels[1])
+    line_2, = ax.plot([], [], 'o', lw=3, label=labels[2])
+
+    ax.set_xlabel('x [unité de distance]')
+    ax.set_ylabel('y [unité de distance]')
+    plt.legend()
+    plt.grid()
+
+    hm_frames: int = min(len(T), 750)
+
+    def init():
+        text_t.set_text(f"t: {0}")
+        line_0.set_data([], [])
+        line_1.set_data([], [])
+        line_2.set_data([], [])
+        return line_0, line_1, line_2, text_t,
+
+    def animate(i):
+        j: int = int(i * (len(T) / hm_frames))
+        text_t.set_text(f"t: {j / len(T):.2f} \n [temps \n normalisé]")
+        line_0.set_data(R[j, 0, 0], R[j, 0, 1])
+        line_1.set_data(R[j, 1, 0], R[j, 1, 1])
+        line_2.set_data(R[j, 2, 0], R[j, 2, 1])
+        return line_0, line_1, line_2, text_t,
+
+    anim = FuncAnimation(fig, animate, init_func=init,
+                         frames=hm_frames, interval=20, blit=True)
+
+    anim.save(f'{titre}.gif', writer='imagemagick')
+    plt.show()
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib.animation import FuncAnimation
+
+    for probleme, donnees in donnee_problemes.items():
+        R, T = resolution_probleme_trois_corps(donnees["m_i"],
+                                               positions_initiales=donnees["r_0"],
+                                               vitesses_initiales=donnees["v_0"],
+                                               bornes=donnees["t"], resolution=10_000)
+        simulation_affichage_3D(R, T, titre=f"simulation_affichage_3D_{probleme}", labels=donnees["labels"])
+        simulaiton_animation_2D(R, T, titre=f"simulation_animation_2D_{probleme}", labels=donnees["labels"])
